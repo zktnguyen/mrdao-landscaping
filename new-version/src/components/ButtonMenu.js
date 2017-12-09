6d93d7457ec584d8c2ba9ca11e3666e77cc3c364
Kim Nguyen <zktnguyen@ucdavis.edu>
2017-12-09 00:39:29 -0800

    added smooth scrolling back-to-top button at the bottom of page

--- new-version/src/components/ButtonMenu.js
+++ new-version/src/components/ButtonMenu.js
@@ -3,10 +3,10 @@ import React from 'react';
 
 const ButtonMenu = () => (
   <div className="button-list animated fadeIn">
-    <button type="button" className="btn btn-outline-success btn-lg">Services</button>
-    <button type="button" className="btn btn-outline-success btn-lg">About</button>
-    <button type="button" className="btn btn-outline-success btn-lg">Schedule</button>
-    <button type="button" className="btn btn-outline-success btn-lg">Contact</button>
+    <button type="button" className="btn btn-secondary btn-lg">Services</button>
+    <button type="button" className="btn btn-secondary btn-lg">About</button>
+    <button type="button" className="btn btn-secondary btn-lg">Schedule</button>
+    <button type="button" className="btn btn-secondary btn-lg">Contact</button>
 
   </div>
 );
--- new-version/src/components/Footer.js
+++ new-version/src/components/Footer.js
@@ -1,51 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { animateScroll as scroll } from 'react-scroll';
 
-const Footer = () => (
-  <footer id="contact">
-    <div className="container">
-      <div className="row">
-        <div className="col">
-          <h2>Contact Info</h2>
-          <ul>
-            <li>
-              <i className="fa fa-phone" id="icons" aria-hidden="true" />
-              {'   '}(510) 786-9459
-            </li>
-            <li>
-              <i className="fa fa-envelope" id="icons" aria-hidden="true" />
-              {'   '}mrdaolandscaping@gmail.com
-            </li>
-          </ul>
+class Footer extends Component {
+  scrollToTop = () => scroll.scrollToTop();
+
+  render() {
+    return (
+      <footer className="container-fluid" id="contact">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h2>Contact Info</h2>
+              <ul>
+                <li>
+                  <i className="fa fa-phone" id="icons" aria-hidden="true" />
+                  {'   '}(510) 786-9459
+                </li>
+                <li>
+                  <i className="fa fa-envelope" id="icons" aria-hidden="true" />
+                  {'   '}mrdaolandscaping@gmail.com
+                </li>
+              </ul>
+            </div>
+            <div className="col">
+              <h2>Contact us anytime!</h2>
+              <p id="important-info">
+                If contacting by phone, please leave a voicemail for inquiries
+                of interest. <br />If sending an email, please include [Service
+                Type] Inquiry in the subject.<br /> Or please try our new
+                scheduling system!
+              </p>
+            </div>
+          </div>
         </div>
-        <div className="col">
-          <h2>Contact us anytime!</h2>
-          <p id="important-info">
-            If contacting by phone, please leave a voicemail for inquiries of
-            interest. <br />If sending an email, please include [Service Type]
-            Inquiry in the subject.<br/> Or please try our new scheduling system!
-          </p>
+        <div className="row copyright text-center">
+          <div className="col-1">
+            <button
+              className="btn btn-secondary"
+              onClick={this.scrollToTop}
+            >
+              Back to Top
+            </button>
+          </div>
+          <div className="col-11">
+            © 2017&nbsp;
+            <a
+              href="http://kimtnguyen.me"
+              rel="noopener noreferrer"
+              target="_blank"
+              style={{ color: 'black' }}
+            >
+              Kim T Nguyen
+            </a>&nbsp;&nbsp;&nbsp;&nbsp;
+            <a
+              href="https://github.com/zktnguyen/portfolio"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              GitHub Repository
+            </a>
+          </div>
         </div>
-      </div>
-      <div className="row copyright">
-        © 2017&nbsp;
-        <a
-          href="http://kimtnguyen.me"
-          rel="noopener noreferrer"
-          target="_blank"
-          style={{color: 'black'}}
-        >
-          Kim T Nguyen
-        </a>&nbsp;&nbsp;&nbsp;&nbsp;
-        <a
-          href="https://github.com/zktnguyen/portfolio"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          GitHub Repository
-        </a>
-      </div>
-    </div>
-  </footer>
-);
+      </footer>
+    );
+  }
+}
 
 export default Footer;
